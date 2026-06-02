700d8996ab21525273becc9809bb7b74135d59bc
Awaken <93601997+Awakenchan@users.noreply.github.com>
2026-06-02 06:40:17 +0500

    Refactor Main.lua for improved configuration handling

--- Main.lua
+++ Main.lua
@@ -1,158 +1,116 @@
---[[
-	⣿⣿⣿⣿⣿⣿SIGMA SPY⣿⣿⣿⣿⣿⣿
-	⣿⣿⣯⡉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁
-	⠉⠻⣿⣿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-	⠀⠀⠈⠻⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-	⠀⠀⠀⠀⠀⠙⢿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀
-	⠀⠀⠀⠀⠀⠀⠀⣉⣿⣿⣿⠆⠀⠀⠀⠀⠀⠀⠀
-	⠀⠀⠀⠀⠀⣠⣾⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀
-	⠀⠀⢀⣴⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-	⣀⣴⣿⣿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-	⣿⣿⣟⣁⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⡀
-	⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
-
-    Written by @depso
-    MIT License
-    
-    https://github.com/depthso
-]]
-
---// File handling configuration 
-local FilesConfig = {
-	UseWorkspace = false,
-	RepoUrl = "https://github.com/Awakenchan/Sigma-Spy" -- "http://127.0.0.1:3000"
+--// Base Configuration
+local Configuration = {
+    UseWorkspace = false, 
+    NoActors = false,
+    FolderName = "Sigma Spy",
+    RepoUrl = [[https://raw.githubusercontent.com/Awakenchan/Sigma-Spy/main]],
+    ParserUrl = [[https://raw.githubusercontent.com/Awakenchan/Sigma-Spy/refs/heads/main/lib/RobloxParser]],
+    Directory = "Sigma Spy"
 }
-
---// Service handlers
+--// Load overwrites
+local Parameters = {...}
+local Overwrites = Parameters[1]
+if typeof(Overwrites) == "table" then
+    for Key, Value in Overwrites do
+        Configuration[Key] = Value
+    end
+end
+--// Service handler
 local Services = setmetatable({}, {
-	__index = function(self, Name: string): Instance
-		local Service = game:GetService(Name)
-		return cloneref(Service)
-	end,
+    __index = function(self, Name: string): Instance
+        local Service = game:GetService(Name)
+        return cloneref(Service)
+    end,
 })
-
+--// Files module
+local Files = loadstring(game:HttpGet(Configuration.RepoUrl.."/lib/Files.lua"))()
+Files:PushConfig(Configuration)
+Files:Init({
+    Services = Services
+})
+local Folder = Files.FolderName
+local Scripts = {
+    --// User configurations
+    Config = Files:GetModule({Folder}/Config, "Config"),
+    ReturnSpoofs = Files:GetModule({Folder}/Return spoofs, "Return Spoofs"),
+    --// Libraries
+    Process = game:HttpGet({Configuration.RepoUrl}/lib/Process.lua),
+    Hook = game:HttpGet({Configuration.RepoUrl}/lib/Hook.lua),
+    Flags = game:HttpGet({Configuration.RepoUrl}/lib/Flags.lua),
+    Ui = game:HttpGet({Configuration.RepoUrl}/lib/Ui.lua),
+    Generation = game:HttpGet({Configuration.RepoUrl}/lib/Generation.lua),
+    Communication = game:HttpGet({Configuration.RepoUrl}/lib/Communication.lua)
+}
 --// Services
 local Players: Players = Services.Players
-
---// Fetch Files module code
-local FilesScript
-if FilesConfig.UseWorkspace then
-	FilesScript = readfile(`{FilesConfig.Folder}/lib/Files.lua`)
-else
-	FilesScript = game:HttpGet(`{FilesConfig.RepoUrl}/lib/Files.lua`)
-end
-
---// Load files module
-local Files = loadstring(FilesScript)()
-Files:PushConfig(FilesConfig)
-Files:Init({
-	Services = Services
-})
-
---// Modules
-local Scripts = {
-	--// User configurations
-	Config = Files:GetModule("Sigma Spy/Config", "Config"),
-	ReturnSpoofs = Files:GetModule("Sigma Spy/Return spoofs", "Return Spoofs"),
-
-	--// Libraries
-	Process = Files:GetModule("lib/Process"),
-	Hook = Files:GetModule("lib/Hook"),
-	Flags = Files:GetModule("lib/Flags"),
-	Ui = Files:GetModule("lib/Ui"),
-	Generation = Files:GetModule("lib/Generation"),
-	Communication = Files:GetModule("lib/Communication")
-}
-
 --// Dependencies
 local Modules = Files:LoadLibraries(Scripts)
+Modules.Files = Files
+Modules.Configuration = Configuration
 local Process = Modules.Process
 local Hook = Modules.Hook
-local Config = Modules.Config
 local Ui = Modules.Ui
 local Generation = Modules.Generation
 local Communication = Modules.Communication
-
---// Unpack config
-local BlackListedServices = Config.BlackListedServices
-
+local Config = Modules.Config
 --// Use custom font (optional)
 local FontContent = Files:GetAsset("ProggyClean.ttf", true)
 local FontJsonFile = Files:CreateFont("ProggyClean", FontContent)
-Ui:SetFont(FontJsonFile, FontContent)
-
---// Actor code
-local ActorCode = Files:CompileModule(Scripts)
-ActorCode ..= [=[
-	local ExtraData = {
-		IsActor = true
-	}
-	print("ChannelId", ...)
-	Libraries.Hook:BeginService(Libraries, ExtraData, ...)
-]=]
-
+Ui:SetFontFile(FontJsonFile)
 --// Load modules
+Process:CheckConfig(Config)
 Files:LoadModules(Modules, {
-	Modules = Modules,
-	Services = Services
+    Modules = Modules,
+    Services = Services,
+    Configuration = Configuration
 })
-
 --// ReGui Create window
-local Window = Ui:CreateWindow()
-
+local Window = Ui:CreateMainWindow()
 --// Check if Sigma spy is supported
 local Supported = Process:CheckIsSupported()
 if not Supported then 
-	Window:Close()
-	return
+    Window:Close()
+    return
 end
-
+--// Create communication channel
+local ChannelId, Event = Communication:CreateChannel()
+Communication:AddCommCallback("QueueLog", function(...)
+    Ui:QueueLog(...)
+end)
+Communication:AddCommCallback("Print", function(...)
+    Ui:ConsoleLog(...)
+end)
 --// Generation swaps
 local LocalPlayer = Players.LocalPlayer
 Generation:SetSwapsCallback(function(self)
-	self:AddSwap(LocalPlayer, {
-		String = "LocalPlayer",
-	})
-	self:AddSwap(LocalPlayer.Character, {
-		String = "Character",
-		NextParent = LocalPlayer
-	})
+    self:AddSwap(LocalPlayer, {
+        String = "LocalPlayer",
+    })
+    self:AddSwap(LocalPlayer.Character, {
+        String = "Character",
+        NextParent = LocalPlayer
+    })
 end)
-
---// Beta alert modal
-Ui:ShowModal({
-	"<b>Attention!</b>",
-	"Sigma Spy is in BETA, please expect issues\n",
-	"Report any issues to the Github page (depthso/Sigma-Spy)\n",
-	"Many thanks!"
-})
-
 --// Create window content
 Ui:CreateWindowContent(Window)
-
---// Create communication channel
-local ChannelId = Communication:CreateChannel()
-Communication:AddCommCallback("QueueLog", function(...)
-	Ui:QueueLog(...)
-end)
-
---// Begin hook
-Hook:BeginService(Modules, nil, ChannelId) -- Run on self
-Hook:RunOnActors(ActorCode, ChannelId) -- Run on actors
-
---// Remote added
-game.DescendantAdded:Connect(function(Remote) -- TODO
-	Hook:ConnectClientRecive(Remote)
-end)
-
---// Collect missing remotes
-Hook:MultiConnect(getnilinstances())
-
---// Search for remotes
-for _, Service in next, game:GetChildren() do
-	if table.find(BlackListedServices, Service.ClassName) then continue end
-	Hook:MultiConnect(Service:GetDescendants())
-end
-
---// Begin the Log queue service
+--// Begin the Log queue 
+Ui:SetCommChannel(Event)
 Ui:BeginLogService()
+--// Load hooks
+local ActorCode = Files:MakeActorScript(Scripts, ChannelId)
+Hook:LoadHooks(ActorCode, ChannelId)
+local EnablePatches = Ui:AskUser({
+    Title = "Enable function patches?",
+    Content = {
+        "On some executors, function patches can prevent common detections that executor has",
+        "By enabling this, it MAY trigger hook detections in some games, this is why you are asked.",
+        "If it doesn't work, rejoin and press 'No'",
+        "",
+        "(This does not affect game functionality)"
+    },
+    Options = {"Yes", "No"}
+}) == "Yes"
+--// Begin hooks
+Event:Fire("BeginHooks", {
+    PatchFunctions = EnablePatches
+})
